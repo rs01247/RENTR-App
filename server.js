2bde5d17c9fb9e6396eb3163a24b3f05422caa6d
Raymond <rs01247@gmail.com>
2018-10-04 20:33:25 -0700

    heroku push

--- server.js
+++ server.js
@@ -61,7 +61,7 @@ app.post("/charge", (req, res) => {
 });
 
 // CHANGE TO FORCE:FALSE BEFORE DEPLOYING
-db.Sequelize.sync({ force: true }).then(function () {
+db.Sequelize.sync({ force: false }).then(function () {
   app.listen(PORT, function () {
     console.log(`Listening to ${PORT}`);
   });
